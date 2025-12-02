acde7d2ef201cb23db298eb0edeed144dcbbef44
jonas-bechtel <jonas.bechtel@t-online.de>
2025-12-02 16:32:01 +0100

    fixed linux run scripts

--- scripts/run-debug.sh
+++ scripts/run-debug.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Running program in Debug"
+echo "Running program in Release"
 
-ROOTSYS=$(dirname "$0")/../vendor/root_v6.32.04
-export PATH="$ROOTSYS/bin:$PATH"
+script_path=$(readlink -f -- "$0")
+script_dir=$(dirname -- "$script_path")
+ROOTSYS=${script_dir}/../vendor/root_v6.32.04
 
-cd "$(dirname "$0")/../Energy-Distribution-Model"
+export LD_LIBRARY_PATH="${ROOTSYS}/lib:${LD_LIBRARY_PATH}"
 
-./bin/Debug-linux-x86_64/Energy-Distribution-Model  # Note the escaping of spaces
+cd "${script_dir}/../Energy-Distribution-Model"
+./../bin/Release-debug-x86_64/Energy-Distribution-Model/Energy-Distribution-Model
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Running program in Release"
 
-ROOTSYS=$(dirname "$0")/../vendor/root_v6.32.04
-export PATH="$ROOTSYS/bin:$PATH"
+script_path=$(readlink -f -- "$0")
+script_dir=$(dirname -- "$script_path")
 
-cd "$(dirname "$0")/../Energy-Distribution-Model"
+ROOTSYS=${script_dir}/../vendor/root_v6.32.04
 
-./bin/Release-linux-x86_64/Energy-Distribution-Model  # Note the escaping of spaces
+#export PATH="$ROOTSYS/bin:$PATH"
+#export PATH="$ROOTSYS/lib:$PATH"
+export LD_LIBRARY_PATH="${ROOTSYS}/lib:${LD_LIBRARY_PATH}"
+echo "${ROOTSYS}/lib"
+
+#cd "${ROOTSYS}"
+#./bin/thisroot.sh
+
+cd "${script_dir}/../Energy-Distribution-Model"
+./../bin/Release-linux-x86_64/Energy-Distribution-Model/Energy-Distribution-Model
