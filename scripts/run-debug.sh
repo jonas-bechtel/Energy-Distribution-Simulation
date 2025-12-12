6a14a54f80ad2907f1458f24f2162918a2efac91
jonas-bechtel <jonas.bechtel@t-online.de>
2025-12-12 15:28:41 +0100

    fixed typo

--- scripts/run-debug.sh
+++ scripts/run-debug.sh
@@ -9,4 +9,4 @@ ROOTSYS=${script_dir}/../vendor/root_v6.32.04
 export LD_LIBRARY_PATH="${ROOTSYS}/lib:${LD_LIBRARY_PATH}"
 
 cd "${script_dir}/../Energy-Distribution-Model"
-./../bin/Release-debug-x86_64/Energy-Distribution-Model/Energy-Distribution-Model
+./../bin/Debug-linux-x86_64/Energy-Distribution-Model/Energy-Distribution-Model
